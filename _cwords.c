3d55a993beadc867d7ca1fd1611825ad32ff3785
aylaaoufi <laaoufi.ayoub@programmer.net>
2024-02-19 09:56:12 +0100

    fix err

--- _cwords.c
+++ _cwords.c
@@ -2,17 +2,17 @@
 
 int count_words(char *buffer)
 {
-        int words = 0, i = 0;
+        int ayoub = 0, i = 0;
 
         if (_strcmp_echo(buffer, NULL))
                 return (count_words_echo(buffer));
         if (buffer[0] != 32)
-                words++;
+                ayoub++;
         while (buffer[i])
         {
                 if (buffer[i] == 32 && buffer[i + 1] != 32 && buffer[i + 1] != '\0')
-                        words++;
+                        ayoub++;
                 i++;
         }
-        return (words);
+        return (ayoub);
 }
