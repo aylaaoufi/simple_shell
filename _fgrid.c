22137e2804bda272fba000440629e43b501df6c1
aylaaoufi <laaoufi.ayoub@programmer.net>
2024-02-20 23:21:56 +0100

    fix betty

--- _fgrid.c
+++ _fgrid.c
@@ -1,12 +1,15 @@
 #include "main.h"
-
+/**
+ * free_grid - Free memory allocated for a 2D array.
+ * @s: The 2D array.
+ */
 void free_grid(char **s)
 {
-        int i = 0;
+        int m = 0;
 
         if (!s)
                 return;
-        for (; s[i]; i++)
-                free(s[i]);
+        for (; s[m]; m++)
+                free(s[m]);
         free(s);
 }
