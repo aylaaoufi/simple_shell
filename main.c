82210b8fe6195ea3f64b1b3fafd723e849ba060d
aylaaoufi <laaoufi.ayoub@programmer.net>
2024-02-20 23:18:50 +0100

    code

--- main.c
+++ main.c
@@ -1,33 +1,40 @@
 #include "main.h"
-
+/**
+ * main - Entry point for the custom shell program.
+ * @ac: The number of command-line arguments.
+ * @av: An array of strings representing the command-line arguments.
+ *
+ * Return: 0 on successful execution,
+ *  or an exit status indicating an error.
+ */
 int main(int ac, char **av)
 {
-	int status = 0, is = 0, exit_s = 0, get_line = 0, is_f = -1;
-	char **s = NULL, *buffer = NULL;
-	size_t len = 0;
+	int ayoub = 0, ll = 0, _exit = 0, line = 0, mm = -1;
+	char **x = NULL, *buffer = NULL;
+	size_t tol = 0;
 
 	(void) ac;
 	if (isatty(STDIN_FILENO) != 0)
 		return (interactive_mode(av[0]));
 	while (1)
 	{
-		get_line = getline(&buffer, &len, stdin);
-		exit_s = redirector(&buffer, get_line, av[0], 0, 1);
-		if (exit_s == 1)
+		line = getline(&buffer, &tol, stdin);
+		_exit = redirector(&buffer, line, av[0], 0, 1);
+		if (_exit == 1)
 			continue;
-		else if (exit_s != -1)
-			return (exit_s);
-		s = allocate_argv_and_set(buffer, &is_f);
-		exit_s = allocat_faild(&s, &buffer, is_f);
-		if (exit_s != 2)
-			return (exit_s);
-		is = path_faild(&buffer, &s, av[0], 0, 1);
-		if (is != -1)
-			return (is);
-		status = new_pro(&s, &buffer, av[0], 0);
-		if (status != -1)
-			return (status);
-		free_grid(s);
+		else if (_exit != -1)
+			return (_exit);
+		x = allocate_argv_and_set(buffer, &mm);
+		_exit = allocat_faild(&x, &buffer, mm);
+		if (_exit != 2)
+			return (_exit);
+		ll = path_faild(&buffer, &x, av[0], 0, 1);
+		if (ll != -1)
+			return (ll);
+		ayoub = new_pro(&x, &buffer, av[0], 0);
+		if (ayoub != -1)
+			return (ayoub);
+		free_grid(x);
 	}
 	return (0);
 }
