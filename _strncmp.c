3b6db6db3c6abda01ef9a9aef3350445bb5685e8
aylaaoufi <laaoufi.ayoub@programmer.net>
2024-02-20 22:53:28 +0100

    add betty

--- _strncmp.c
+++ _strncmp.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * _strncmp - Compare the first n characters of two strings.
+ * @s1: The first string to compare.
+ * @s2: The second string to compare.
+ * @n: The number of characters to compare.
+ * Return: 0 if Success, -1 Failure.
+ */
 int _strncmp(char *s1, char *s2, int n)
 {
         int x = 0;
