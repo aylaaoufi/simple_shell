580767fa70041ffb7c640dbda775613126b45e38
aylaaoufi <laaoufi.ayoub@programmer.net>
2024-02-20 22:48:50 +0100

    fix format

--- _strcat.c
+++ _strcat.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * _strcat - concatenates two strings
  * @dest: pointer to destination
  * @src: pointer to source
  * Return: Destination
  */
-
 char *_strcat(char *dest, char *src)
 {
         int a = 0;
