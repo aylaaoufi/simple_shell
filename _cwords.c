0d694d07680d1e3c73461d66316812efcfe19cfe
aylaaoufi <laaoufi.ayoub@programmer.net>
2024-02-21 01:39:10 +0100

    fix betty

--- _cwords.c
+++ _cwords.c
@@ -1,18 +1,22 @@
 #include "main.h"
-
+/**
+ * count_words - Count the number of words in a string.
+ * @buffer: The input string.
+ * Return: The number of words in the string.
+ */
 int count_words(char *buffer)
 {
-        int ayoub = 0, i = 0;
+        int ayoub = 0, x = 0;
 
         if (_strcmp_echo(buffer, NULL))
                 return (count_words_echo(buffer));
         if (buffer[0] != 32)
                 ayoub++;
-        while (buffer[i])
+        while (buffer[x]) /*"   this     is a example " = 4*/
         {
-                if (buffer[i] == 32 && buffer[i + 1] != 32 && buffer[i + 1] != '\0')
+                if (buffer[x] == 32 && buffer[x + 1] != 32 && buffer[x + 1] != '\0')
                         ayoub++;
-                i++;
+                x++;
         }
         return (ayoub);
 }
