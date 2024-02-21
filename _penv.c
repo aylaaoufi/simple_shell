4410256848e2864cf46cfcbfa9b6657d063c8df4
aylaaoufi <laaoufi.ayoub@programmer.net>
2024-02-21 02:20:20 +0100

    wa 3yeeeeeeet

---
+++ _penv.c
@@ -0,0 +1,16 @@
+#include "main.c"
+/**
+ * print_env - Print the environment variables to STDOUT.
+ */
+void print_env(void)
+{
+	size_t len;
+	int p = 0;
+
+	for (; environ[p] != NULL; p++)
+	{
+		len = _strlen(environ[p]);
+		write(STDOUT_FILENO, environ[p], len);
+		write(STDOUT_FILENO, "\n", 1);
+	}
+}
