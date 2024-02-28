f6d29067e4233d4808f41905e71b5a1de7f222cf
aylaaoufi <ayoub.laaoufi@hotmail.com>
2024-02-28 01:56:51 +0100

    fixing all errors

--- main.h
+++ main.h
@@ -1,5 +1,8 @@
 #ifndef _SHELL_
 #define _SHELL_
+
+#ifndef _SHELL_
+#define _SHELL_
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
@@ -10,36 +13,42 @@
 #include <sys/stat.h>
 
 extern char **environ;
-int berror(int count, char *buffer, char *av);
-int berror_exit(int count, char *n, char *av);
-void _puts(char *str);
-void print_int(unsigned int n);
-int interactive_mode(char *av);
-char **allocate_argv_and_set(char *buffer, int *f);
-char *alloc_words_buffer(char *buffer, int *index_buffer);
-int allocat_faild(char ***s, char **buffer, int is_f);
+/*========================== Memory functions ============================*/
 void free_grid(char **s);
-int _strlen(char *str);
-int count_words_echo(char *buffer);
-int count_words(char *buffer);
-char *_strchr(char *s, char c);
-char *_strcpy(char *dest, char *src);
-char *_strcat(char *dest, char *src);
+char **allocate_argv_and_set(char *buffer, int *f);
+int allocat_faild(char ***s, char **buffer, int is_f);
+char *alloc_words_buffer(char *buffer, int *index_buffer);
+char *alloc_words_echo(char *buffer, int *index_buffer);
+/*======================= End of memory functions ========================*/
+/*============================ STR functions =============================*/
+char *_strtok(char *str, char *dahawmitr);
+char *_strdup(char *buffer);
 char *cut_the_first_word(char *buffer);
 int _strcmp_exit(char *dest);
 int _atoi(char *s);
-void _putchar(char c);
-int _strcmp(char *src, char *dest);
-int _strcmp_echo(char *dest, char *src);
 char *skip_space_tab_for_berror(char *buffer);
-char *_strtok(char *str, char *dahawmitr);
-char *_strdup(char *buffer);
+void _putchar(char c);
+int count_words_echo(char *buffer);
+int count_words(char *buffer);
+char *_strcpy(char *dest, char *src);
+int berror(int count, char *buffer, char *av);
+int berror_exit(int count, char *n, char *av);
+int _strlen(char *str);
+char *_strchr(char *s, char c);
+int _strcmp(char *src, char *dest);
+char *_strcat(char *dest, char *src);
+int _strcmp_echo(char *dest, char *src);
+void _puts(char *str);
+void print_int(unsigned int n);
+/*======================== End of STR functions ==========================*/
 int new_pro(char ***s, char **buffer, char *av, int c);
-char *real_path(char **buffer, int *faild);
+int interactive_mode(char *av);
+/*============================ path functions =============================*/
 int path_faild(char **buffer, char ***s, char *av, int c, int i);
-int redirector(char **buffer, int get_line, char *av, int c, int i);
 char *_getenvi();
 void print_env(void);
+char *real_path(char **buffer, int *faild);
+int redirector(char **buffer, int get_line, char *av, int c, int i);
 int advnce_exit(char *buffer, char *av);
-char *alloc_words_echo(char *buffer, int *index_buffer);
+/*======================== End of path functions ==========================*/
 #endif
